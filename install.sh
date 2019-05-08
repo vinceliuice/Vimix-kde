41362387d571c192ce0d77866362b9d68ca1e172
vinceliuice <vinceliuice@hotmail.com>
2019-05-08 18:29:03 +0800

    Add light and dark versions

--- install.sh
+++ install.sh
@@ -9,19 +9,19 @@ LOOKFEEL_DIR="$HOME/.local/share/plasma/look-and-feel"
 KVANTUM_DIR="$HOME/.config/Kvantum"
 
 THEME_NAME=Vimix
-COLOR_VARIANTS=('' '-Dark')
+COLOR_VARIANTS=('' '-Light' '-Dark')
 THEME_VARIANTS=('' '-Doder' '-Beryl' '-Ruby')
 
 install() {
   local name=${1}
-  local theme=${2}
-  local color=${3}
+  local color=${2}
+  local theme=${3}
 
-  local AURORAE_THEME=${AURPRAE_DIR}/${name}${theme}
+  local AURORAE_THEME=${AURPRAE_DIR}/${name}${color}${theme}
   local PLASMA_THEME=${PLASMA_DIR}/${name}${theme}
   local LOOKFEEL_THEME=${LOOKFEEL_DIR}/${name}${theme}
-  local SCHEMES_THEME=${SCHEMES_DIR}/${name}${theme}${color}.colors
-  local KVANTUM_THEME=${KVANTUM_DIR}/${name}${theme}
+  local SCHEMES_THEME=${SCHEMES_DIR}/${name}${color}${theme}.colors
+  local KVANTUM_THEME=${KVANTUM_DIR}/${name}${color}${theme}
 
   [[ -d ${AURORAE_THEME} ]] && rm -rf ${AURORAE_THEME}
   [[ -d ${PLASMA_THEME} ]] && rm -rf ${PLASMA_THEME}
@@ -29,20 +29,25 @@ install() {
   [[ -d ${SCHEMES_THEME} ]] && rm -rf ${SCHEMES_THEME}
   [[ -d ${KVANTUM_THEME} ]] && rm -rf ${KVANTUM_THEME}
 
-  cp -ur ${SRC_DIR}/aurorae/${name}${theme}                                          ${AURORAE_DIR}
-  cp -ur ${SRC_DIR}/color-schemes/${name}${theme}${color}.colors                     ${SCHEMES_DIR}
-  cp -ur ${SRC_DIR}/Kvantum/${name}${theme}                                          ${KVANTUM_DIR}
-  cp -ur ${SRC_DIR}/plasma/desktoptheme/${name}${theme}                              ${PLASMA_DIR}
-  cp -ur ${SRC_DIR}/plasma/desktoptheme/icons                                        ${PLASMA_THEME}
-  cp -ur ${SRC_DIR}/color-schemes/${name}${theme}-Dark.colors                        ${PLASMA_THEME}/colors
-  cp -ur ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${theme}      ${LOOKFEEL_DIR}
+  if [[ ${color} != '-Light' ]]; then
+    cp -ur ${SRC_DIR}/aurorae/${name}${theme}                                           ${AURORAE_DIR}
+  else
+    cp -ur ${SRC_DIR}/aurorae/${name}-Light                                             ${AURORAE_DIR}
+  fi
+
+  cp -ur ${SRC_DIR}/color-schemes/${name}${color}${theme}.colors                        ${SCHEMES_DIR}
+  cp -ur ${SRC_DIR}/Kvantum/${name}${color}${theme}                                     ${KVANTUM_DIR}
+  cp -ur ${SRC_DIR}/plasma/desktoptheme/${name}${theme}                                 ${PLASMA_DIR}
+  cp -ur ${SRC_DIR}/plasma/desktoptheme/icons                                           ${PLASMA_THEME}
+  cp -ur ${SRC_DIR}/color-schemes/${name}-Dark${theme}.colors                           ${PLASMA_THEME}/colors
+  cp -ur ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${color}${theme} ${LOOKFEEL_DIR}
 }
 
 echo "Installing '${name:-${THEME_NAME}} kde themes'..."
 
-for theme in "${themes[@]:-${THEME_VARIANTS[@]}}"; do
 for color in "${colors[@]:-${COLOR_VARIANTS[@]}}"; do
-  install "${name:-${THEME_NAME}}" "${theme}" "${color}"
+for theme in "${themes[@]:-${THEME_VARIANTS[@]}}"; do
+  install "${name:-${THEME_NAME}}" "${color}" "${theme}"
 done
 done
 
